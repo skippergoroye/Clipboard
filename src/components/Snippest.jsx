2cc87745663b7b5eeb9920caf6de32a8641ad298
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-04-22 14:55:50 -0700

    snippest added

--- src/components/Snippest.jsx
+++ src/components/Snippest.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Snippest = () => {
   return (
-    <div>Snippest</div>
+    <>
+      <section>
+      <div class="section-container my-20">
+          <h3>Keep track of your snippest</h3>
+
+          <p class="section-content mb-24 text-xl">Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.</p>
+      </div>
+     </section>
+    </>
   )
 }
 
