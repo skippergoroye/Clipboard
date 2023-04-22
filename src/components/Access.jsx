ee0e73c9f0ae84b66954447a881a0ed60547f0be
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-04-22 14:59:42 -0700

    Access  added

--- src/components/Access.jsx
+++ src/components/Access.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Devices from '../images/image-devices.png'
 
 const Access = () => {
   return (
-    <div>Access</div>
+    <>
+     <section id="access">
+      <div class="section-container my-20">
+        <h3>Acess Clipboard Anywhere</h3>
+        <p class="section-content mb-24 text-xl">
+          Whether you’re on the go, or at your computer, you can access all your
+          Clipboard snippets in a few simple clicks.
+        </p>
+
+        <img src={Devices} alt="" class="mx-auto" />
+      </div>
+    </section>
+    </>
   )
 }
 
