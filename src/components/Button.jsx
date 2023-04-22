22b02532666bd9f1a82f4571b5756b3dd56165ae
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-04-22 15:10:40 -0700

    button and refernce added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <> 
+      <section id="button">
+      <div class="section-container my-20">
+        <h3>Clipboard for iOS and MacOs</h3>
+        <p class="section-content mb-10 text-xl">
+          Available for free on the App Store. Download for Mac or iOS, sync
+          with iCloud and you're ready to start adding to your clipboard.
+        </p>
+
+        {/* <!--------- Button container -------> */}
+          <div class="button-container">
+            <a href="" class="p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80">Download on IOS</a>
+            <a href="" class="p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80">Download for Mac</a>
+          </div>
+      </div>
+    </section>
+    </>
   )
 }
 
--- src/components/References.jsx
+++ src/components/References.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Google from '../images/logo-google.png'
+import Ibm from '../images/logo-ibm.png'
+import Microsoft from '../images/logo-microsoft.png'
+import Hp from '../images/logo-hp.png'
+import Vector from '../images/logo-vector-graphics.png'
 
 const References = () => {
   return (
-    <div>References</div>
+    <>
+    <section id="references">
+      <div class="flex flex-col items-center justify-between max-w-6xl px-10 mx-auto space-y-16 my-44 md:flex-row md:space-y-0">
+        <img src={Google} alt="" />
+        <img src={Ibm} alt="" />
+        <img src={Microsoft} alt="" />
+        <img src={Hp} alt="" />
+        <img src={Vector} alt="" />
+      </div>
+    </section>
+    </>
   )
 }
 
